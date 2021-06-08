a4fce0f94aecba66cb04befda6dcfd9b4589c59c
0-null-0 <offficialstudy@gmail.com>
2021-06-08 14:48:52 +0530

    Files added to the theory

---
+++ begginer/File_handling_with_python/Codes/Opening_files/open_files.py
@@ -0,0 +1,36 @@
+# Write a python program to open and then close a file
+'''
+file_objet=open('today.txt')
+file_object.close()
+'''
+
+# Write a python program to open a user defined file(user must give relative path of the file)
+'''
+file_objet=open(str(input('Enter the relative file path : ')))
+print('file successfully opened')
+file_object.close()
+'''
+
+# Write a python program to open a user defined file(user must give absolute path of the file)
+'''
+file_objet=open(str(input('Enter the absolute file path : ')))
+print('file successfully opened')
+file_object.close()
+'''
+
+
+# advanced
+# Write a python program to open a user defined file(user must give absolute path of the file)
+'''
+def pathprovider(n=int(input('press 1. for relative path \npress 2. for absolute paht\nEnter the option : '))):
+	if n in (1,2):
+		if n==1:
+			return path=input('Enter the path : ')
+		else :
+			return path=input('Enter the path : ').replace('\\',r'\\')
+	else: print('invalid input')
+
+file_objet=open(pathprovider())
+print('file exists')
+file_object.close()
+'''
---
+++ begginer/File_handling_with_python/Codes/Writting_and_appending/appending.py
@@ -0,0 +1,15 @@
+#python to accept roll no, name and class of 5 student in a class store it in a file student.
+'''
+def main():
+    count=1
+    while count<=5:
+        stud_name=input('Enter the name : ')
+        stud_class=input('Enter the class : ')
+        stud_roll=input('Enter the roll : ')
+        record=[stud_roll,' ',stud_name,' ',stud_class,' ','\n']
+        pointer.writelines(record)
+        count+=1
+
+with open('school.txt','a') as pointer :
+    main()
+'''
